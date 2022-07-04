87a876d1e84af86f49feb75dab08eb9c14e337cc
sungju-kim <wnwn0719@gmail.com>
2022-07-04 21:14:07 +0900

    Feat: MultiSection Multi Layout

--- CollectionViewPractice/CollectionViewPractice/LayoutFactory.swift
+++ CollectionViewPractice/CollectionViewPractice/LayoutFactory.swift
@@ -9,9 +9,80 @@ import UIKit
 
 enum LayoutFactory {
     static func create() -> UICollectionViewCompositionalLayout {
+        return UICollectionViewCompositionalLayout { (sectionNumber, _) -> NSCollectionLayoutSection? in
+            let section: NSCollectionLayoutSection
+
+            switch sectionNumber {
+            case 0:
+                section = createRedSection()
+            case 1:
+                section = createGreenSection()
+            case 2:
+                section = createYellowSection()
+            default:
+                section = createRedSection()
+            }
+            return section
+        }
+    }
+
+    static func createRedSection() -> NSCollectionLayoutSection {
         let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.5)
+        )
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(
+            top: 10,
+            leading: 10,
+            bottom: 10,
+            trailing: 10
+        )
+
+        let groupSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalWidth(0.5)
+        )
+
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 2)
+        let section = NSCollectionLayoutSection(group: group)
+
+        section.orthogonalScrollingBehavior = .continuous
+        return section
+    }
+
+    static func createGreenSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(0.5)
+        )
+
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        item.contentInsets = .init(
+            top: 10,
+            leading: 10,
+            bottom: 10,
+            trailing: 10
+        )
+
+        let groupSize = NSCollectionLayoutSize(
             widthDimension: .fractionalWidth(0.5),
             heightDimension: .fractionalWidth(0.5)
+        )
+
+        let group = NSCollectionLayoutGroup.vertical(layoutSize: groupSize, subitem: item, count: 2)
+        let section = NSCollectionLayoutSection(group: group)
+
+        section.orthogonalScrollingBehavior = .continuous
+
+        return section
+    }
+
+    static func createYellowSection() -> NSCollectionLayoutSection {
+        let itemSize = NSCollectionLayoutSize(
+            widthDimension: .fractionalWidth(1.0),
+            heightDimension: .fractionalHeight(1.0)
         )
 
         let item = NSCollectionLayoutItem(layoutSize: itemSize)
@@ -29,6 +100,7 @@ enum LayoutFactory {
         let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitems: [item])
         let section = NSCollectionLayoutSection(group: group)
 
-        return UICollectionViewCompositionalLayout(section: section)
+        section.orthogonalScrollingBehavior = .continuous
+        return section
     }
 }
