5a2c4bb467e0923592a7f10f24fb82c8a751e602
sungju-kim <wnwn0719@gmail.com>
2022-07-04 17:31:30 +0900

    프로젝트 초기 설정

---
+++ CollectionViewPractice/CollectionViewPractice/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CollectionViewPractice
+//
+//  Created by dale on 2022/07/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
